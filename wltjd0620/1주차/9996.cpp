1b55e996f2a20f278807534de4ff58ce0c1ce5a2
Jisung Kim <wltjd0620@naver.com>
2025-01-02 23:58:21 +0900

    0102

--- wltjd0620/1주차/9996.cpp
+++ wltjd0620/1주차/9996.cpp
@@ -13,20 +13,21 @@ int main(){
     l = pattern.find('*');
     pattern_prefix = pattern.substr(0,l);
     pattern_suffix = pattern.substr(l+1);
+    
     for(int i = 0; i<N; i++){
         cin >> k;
-        // pattern과 name[i]의 pre,suf 구하기
         
-        name_prefix = k.substr(0,l);
-        name_suffix = k.substr(k.size()-pattern_suffix.size());
-
+        
         //cout << pattern_prefix << " " << pattern_suffix << " " << name_prefix << " " << name_suffix << "\n";
         // 패턴이랑 맞는지 확인
         // 맞으면 "DA" 출력, 틀렸으면 "NE" 출력
         if(pattern_prefix.size() + pattern_suffix.size() > k.size()){
-            cout << "NE" << "\n";
+            cout << "NE\n";
         }
         else{
+            // pattern과 name[i]의 pre,suf 구하기
+            name_prefix = k.substr(0,l);
+            name_suffix = k.substr(k.size() - pattern_suffix.size());
             if(pattern_prefix == name_prefix && pattern_suffix == name_suffix){
                 cout << "DA" << "\n";
             }else cout << "NE" << "\n";
